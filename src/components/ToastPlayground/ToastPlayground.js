501ebbbcd60d980acfd68bc4abffc0ee7c3e0d22
samuel-eric <samueltan086@gmail.com>
2026-06-28 15:01:13 +0700

    define prop for Toast component

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Button from "../Button";
+import Toast from "../Toast";
 
 import styles from "./ToastPlayground.module.css";
 
@@ -9,6 +10,7 @@ const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 function ToastPlayground() {
 	const [message, setMessage] = React.useState("");
 	const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+	const [isToastShowing, setIsToastShowing] = React.useState(false);
 
 	return (
 		<div className={styles.wrapper}>
@@ -16,6 +18,12 @@ function ToastPlayground() {
 				<img alt="Cute toast mascot" src="/toast.png" />
 				<h1>Toast Playground</h1>
 			</header>
+
+			{isToastShowing && (
+				<Toast variant={variant} handleDismiss={() => setIsToastShowing(false)}>
+					{message}
+				</Toast>
+			)}
 
 			<div className={styles.controlsWrapper}>
 				<div className={styles.row}>
@@ -58,7 +66,7 @@ function ToastPlayground() {
 				<div className={styles.row}>
 					<div className={styles.label} />
 					<div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-						<Button>Pop Toast!</Button>
+						<Button onClick={() => setIsToastShowing(true)}>Pop Toast!</Button>
 					</div>
 				</div>
 			</div>
